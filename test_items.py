a98c32df47c6dabfda0054fe8bf7bf6f9b55faed
Асет Копбаев <a.kopbayev@infin-it.kz>
2020-04-08 12:39:54 +0600

    Autotest code for website locale check

--- test_items.py
+++ test_items.py
@@ -1,1 +1,9 @@
+import pytest
+import time
 
+def test_user_should_see_add_to_cart_button(browser):
+    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(url)
+    time.sleep(10)
+    button = len(browser.find_elements_by_css_selector("#add_to_basket_form > button"))
+    assert button > 0, 'Кнопка добавления в Корзину отсутствует!'
